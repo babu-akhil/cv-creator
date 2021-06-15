76203597c0f871d0e8f9a165b0c88358081c75d1
Akhil Babu <realakhilbabu@gmail.com>
2021-06-15 19:52:50 +0530

    wrote generalindo render

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
+import React, {Component} from "react";
+import { FontAwesomeIcon } from '../node_modules/@fortawesome/react-fontawesome'
+import { faAngleRight } from '../node_modules/@fortawesome/free-solid-svg-icons'
+
+
+class GeneralInfo extends Component {
+
+  render() {
+    return (
+      <div id = 'GeneralInfo'>
+        <h2>General Information</h2>
+        <ul>
+          <li><FontAwesomeIcon icon={faAngleRight} id='name' className = 'visible'/> name: </li>
+          <li><FontAwesomeIcon icon={faAngleRight} id='email' className = 'hidden'/> email: </li>
+          <li><FontAwesomeIcon icon={faAngleRight} id='phone' className = 'hidden'/> phone no: </li>
+          </ul> 
+      </div>
+    )
+  }
+
+}
+
+class WorkExperience extends Component {
+  render() {
+    return 
+  }
+}
+
+
+
+
+
 
 
 function App() {
-  return ;
+  return (
+    <div id = 'title'>
+      <h1> CV Creator</h1>
+      <GeneralInfo />
+    </div>
+  )
 }
 
 export default App;
