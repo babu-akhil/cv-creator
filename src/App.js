805f704d7811489f52d49841484a73c8dd0609be
Akhil Babu <realakhilbabu@gmail.com>
2021-06-16 05:05:41 +0530

    save workexp

--- src/App.js
+++ src/App.js
@@ -17,7 +17,6 @@ class GeneralInfo extends Component {
   }
 
   handleKeyDown(event) {
-    console.log(event.key)
     if(event.key == 'Enter')
     {
       const form = event.target.form;
@@ -94,11 +93,45 @@ class GeneralInfo extends Component {
 class WorkExperience extends Component {
   constructor(props) {
     super(props)
-    this.state = {items: [], input: {position: '', company :'', description: '', duration: ''}, mode: 'edit'}
+    this.state = {items: [], input: {position: '', company :'', description: '', startDate: '', endDate: '', ongoing: ''}, mode: 'edit'}
+    this.handleKeyPress = this.handleKeyPress.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSave = this.handleSave.bind(this)
   }
 
+  handleChange(event) {
+    if(event.target.name ==='ongoing') {
+      const prevInput = this.state.input
+      const updateThis = event.target.name
+      this.setState({input: {...prevInput, [updateThis]: event.target.checked}})
+    }
+    else {
+      const prevInput = this.state.input
+      const updateThis = event.target.name
+      this.setState({input: {...prevInput, [updateThis]: event.target.value}})
+    }
+  } 
+  
+  handleKeyPress(event) {
+    if(event.key==='Enter') {
+      this.handleChange(event)
+      const form = event.target.form;
+      const index = Array.prototype.indexOf.call(form, event.target);
+      console.log(form)
+      if(form.elements[index+1])
+        {
+        form.elements[index + 1].focus();
+      }
+      else {this.setState({mode: 'view'});
+            console.log(this.state)}
+    }
+  }
 
-
+  handleSave() {
+    let inputs = this.state.input
+    let newItems = [...this.state.items, inputs]
+    this.setState({items: newItems, mode:'view'})
+  }
 
   render() {
 
@@ -106,41 +139,74 @@ class WorkExperience extends Component {
       return (
         <div id = 'WorkExperience' className = 'nes-container with-title'>
           <h2 className = 'title'>Work Experience</h2>
+          <form>
           <div className = "nes-field">
             <label htmlFor ="role">role: </label>
-            <input type = "text" id = "roleInput" className = "nes-input" placeholder="unpaid intern? :(" name='role'></input>
+            <input type = "text" id = "roleInput" className = "nes-input" placeholder="unpaid intern? :(" name='position' onChange = {this.handleChange} onKeyDown = {this.handleKeyPress}></input>
           </div>
           <div className = "nes-field">
             <label htmlFor = 'company'>company: </label>
-            <input type = "text" id = "companyInput" className = "nes-input" placeholder="Broke ass $tartup? :/" name='company'></input>
+            <input type = "text" id = "companyInput" className = "nes-input" placeholder="Broke ass $tartup? :/" name='company' onChange = {this.handleChange} onKeyDown = {this.handleKeyPress}></input>
           </div>
           <div className = "nes-field">
             <label htmlFor = 'description'>describe your role: </label>
-            <textarea id = "descriptionInput" rows = '5' className = "nes-textarea" name = 'description' placeholder="trying to understand old codebases..."></textarea>
+            <textarea id = "descriptionInput" rows = '5' className = "nes-textarea" name = 'description' placeholder="trying to understand old codebases..." onChange = {this.handleChange} onKeyDown = {this.handleKeyPress}></textarea>
           </div>
             <div id = 'startDate'>
               <label htmlFor = 'startDate'>Joining date: </label>
-              <input type = 'month' className = 'nes-input' name = 'startDate' id = 'startDateInput'></input>
+              <input type = 'month' className = 'nes-input' name = 'startDate' id = 'startDateInput' onChange = {this.handleChange}></input>
             </div>
             <div id = 'endDate'>
               <label htmlFor = 'endDate'>Leaving Date: </label>
-              <input type = 'month' className = 'nes-input' name = 'endDate' id = 'endDateInput'></input>
+              <input type = 'month' className = 'nes-input' name = 'endDate' id = 'endDateInput' onChange = {this.handleChange}></input>
             </div>
             <div className = 'nes-field'>
               <label>
-                <input type = "checkbox" className = "nes-checkbox" id = 'currentWork'></input>
+                <input type = "checkbox" className = "nes-checkbox" id = 'currentWork' onChange = {this.handleChange} name = 'ongoing'></input>
                 <span>Currently employed</span>
               </label>
             </div>
-            <div className = 'buttons'>
+            </form>
+            <div className = 'buttons'>    
               <button id = 'savebtn' onClick = {this.handleSave} className = 'nes-btn is-success'>SAVE</button>
-              <button id = 'addbtn' onClick = {this.handleAdd} className = 'nes-btn is-primary'>Add Another</button>
+              <button id = 'addbtn' onClick = {this.handleAdd} className = 'nes-btn is-primary'>Add Another</button>          
             </div>
+          
         </div>
       )
+      }
+      else if(this.state.mode == 'view') {
+          const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September',
+                          'October', 'November', 'December']
+          return (this.state.items.map(item => { 
+            let endDate = item.endDate
+            let startDate = new Date(item.startDate)
+            let startDateString = months[startDate.getMonth()]+', '+ startDate.getFullYear()
+            let endDateString = ''
+            if (item.endDate==''){endDateString = 'Present'}
+            else {endDate = new Date(item.endDate)
+                  endDateString = months[endDate.getMonth()]+', '+ endDate.getFullYear()
+                  }
+            return(
+            <div id = 'WorkExperience' className = 'nes-container with-title'>
+              <h2 className = 'title'>Work Experience</h2>
+              <ul>
+                <li>role: {item.position}</li>
+                <li>company: {item.company}</li>
+                <li>job description: {item.description}</li>
+                <li>duration: {startDateString} to {endDateString}</li>
+              </ul>
+              <div className = 'buttons'>
+                  
+                <button id = 'addbtn' onClick = {this.handleAdd} className = 'nes-btn is-primary'>Add Another</button>
+              </div>
+            </div>
+          )
+        }))
+      }
     }
-  }
 }
+
 
 
 
@@ -149,7 +215,7 @@ class WorkExperience extends Component {
 
 function App() {
   return (
-    <div id = 'title'>
+    <div id = 'everything'>
       <h1> CV Creator</h1>
       <GeneralInfo />
       <WorkExperience />
